d16a8e141b542ee6e376ba85c06998f8c2383fa0
Matheus Coelho <coelho284@gmail.com>
2025-08-07 12:16:16 -0300

    integracao supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
 
-function App() {
+import { useState, useEffect } from 'react'
+import supabase from '@utils/supabase'
+
+function Page() {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    async function getTodos() {
+      const { data: todos } = await supabase.from('todos').select()
+
+      if (todos.length > 1) {
+        setTodos(todos)
+      }
+    }
+
+    getTodos()
+  }, [])
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <div>
+      {todos.map((todo) => (
+        <li key={todo}>{todo}</li>
+      ))}
+    </div>
   )
 }
-
-export default App
+export default Page
