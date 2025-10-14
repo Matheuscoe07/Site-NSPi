c88e9bbd387f57ab319ddf277151f0081452828b
Retr0 <eikebarbos@gmail.com>
2025-10-14 10:38:09 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
-import https from 'https';
-import fs from 'fs';
+import http from 'http';
 import express from 'express';
 
 const app = express();
 
 // exemplo básico
 app.get('/', (req, res) => {
-  res.send('Site HTTPS no ar 😎');
+  res.send('Site HTTP no ar 🚀');
 });
 
-const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/seu-dominio.com/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/seu-dominio.com/fullchain.pem')
-};
-
-https.createServer(options, app).listen(443, () => {
-  console.log('Servidor HTTPS rodando na porta 443 🔒');
+// cria o servidor HTTP
+http.createServer(app).listen(80, () => {
+  console.log('Servidor HTTP rodando na porta 80 🌐');
 });
